8db67d47706eba4fec43beb618725cb7fe22e1ff
Alexis Ruales <anderson.ruales@udea.edu.co>
2020-02-18 00:23:21 -0500

    hola mundo

---
+++ Documentos/clase0/CC1143966473/hello.cpp
@@ -0,0 +1,19 @@
+//comentario
+
+#include <iostream> // usamos la clase de ntrada y salida de flujo, siempre
+/*comentario
+largo
+*/
+
+using namespace std; //esto hace que luego no coloquemos los ::
+
+
+int main()
+  {
+    
+    //instruccione
+    //std::cout<<"Bienvenido a c++"<<std::endl; //salida en terminal
+    cout<<"Bienvenido a c++"<<endl; //salida en terminal
+
+    return 0; //termino bien 
+  }
