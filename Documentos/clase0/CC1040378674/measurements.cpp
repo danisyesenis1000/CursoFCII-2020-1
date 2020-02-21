bd7600764dfb216c7ec38b5fed45d5908f131eee
Luis Papiernik <lpapiernik24@gmail.com>
2020-02-20 21:00:52 -0500

    feat: se termina tarea de seguimiento

--- Documentos/clase0/CC1040378674/measurements.cpp
+++ Documentos/clase0/CC1040378674/measurements.cpp
@@ -16,6 +16,7 @@ float get_area(float length, float width, float depth){
     return 2 * (length + width) * depth + length * width;
 }
 
+
 void user_input(){
     float width;
     float length;
@@ -45,26 +46,35 @@ void user_input(){
     std::cout << '\n';
 }
 
+
+void print_horizontal_line(){
+    std::cout << "---------" << "--------" << "--------------" << "------------" <<
+        "----------" << "---------------------------------" << '\n';
+}
+
 void default_values(){
     float width[8] = {25, 25, 25, 25, 30, 30, 30, 30};
     float length[8] = {10, 10, 10, 10, 12, 12, 12, 12};
     float depth[8] = {5.0, 5.5, 6.0, 6.5, 5.0, 5.5, 6.0, 6.5};
 
-    std::cout << "---------" << "--------" << "--------------" << "------------" <<
-        "----------" << "---------------------------------" << '\n';
+    print_horizontal_line();
 
     std::cout << "| Largo |" << " Ancho |" << " Profundidad |" << " Perimetro |" <<
         " Volumen |" << " Area de superficie subterranea |" << '\n';
 
-    std::cout << "---------" << "--------" << "--------------" << "------------" <<
-        "----------" << "---------------------------------" << '\n';
+    print_horizontal_line();
 
     for(int i = 0; i < 8; i++){
-        std::cout << '|' << std::setw(9 - 2) << length[i] << '|' << std::setw(8 - 1) << width[i] << '|' << std::setw(14 - 2) << depth[i] << "|\n";
+        std::cout << "|" << std::setw(9 - 4) << length[i] <<
+            "  |" << std::setw(8 - 3) << width[i] <<
+            "  |" << std::setw(14 - 3) << depth[i] <<
+            "  |" << std::setw(12 - 3) << get_perimeter(length[i], width[i]) <<
+            "  |" << std::setw(10 - 3) << get_volume(length[i], width[i], depth[i]) <<
+            "  |" << std::setw(34 - 4) << get_area(length[i], width[i], depth[i]) <<
+            "  |\n";
     }
 
-    std::cout << "---------" << "--------" << "--------------" << "------------" <<
-        "----------" << "---------------------------------" << '\n';
+    print_horizontal_line();
 }
 
 
