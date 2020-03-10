35edbcd1324d57871f5089138814f63977ea1bfc
Andres felipe lopez loaiza <36173508+andresfelipelopez@users.noreply.github.com>
2020-03-09 19:46:52 -0500

    Seguimiento parte 1 - clase Rectangulo

---
+++ Documentos/clase0/CC1152215314/clase1_main.cpp
@@ -0,0 +1,14 @@
+#include "clase1.h"
+
+int main()
+{
+  Rectangulo R1;
+  R1.asignarAncho(3.5);
+  R1.asignarLargo(6.5);
+  R1.area();
+  R1.perimetro();
+  R1.obtenerAncho();
+  R1.obtenerLargo();
+  R1.mostrarDatos();
+  return 0;
+}
