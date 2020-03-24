b16c573079e408289c95e0862f8b0055a102d4a5
Carolina Herrera <carolinaherrerasegura@gmail.com>
2020-03-24 17:34:10 -0500

    parcial 1

---
+++ Documentos/Parcial1/CC1037658939/punto_1.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <ctime>
+
+/*
+ * punto1.cpp
+ * 
+ * Game in which the player guesses a number
+ * between 1 and 1000.
+ * 
+ * Carolina Herrera S.
+ * 
+ * */
+
+
+using namespace std;
+
+void game();
+
+int main(){
+    int t = 1;
+    
+    srand(time(NULL));
+    
+    do { 
+        game();
+        cout << endl;
+        cout << "Do you want to play again? Enter 1 for yes, any other number otherwise: ";
+        cin >> t;
+        } while ( t == 1 );
+    
+    return 0;
+}
+
+
+void game(){
+    int number;
+    int guess=0;
+    
+    number = 1+rand()%1000;
+    
+    cout << "Welcome to the game. Guess a number between 1 and 1000." << endl;
+    
+    // Iterates as long as number is different from guess
+    while ( number != guess ){
+        cout << endl;
+        cout << "Enter your guess: ";
+        cin >> guess;
+        
+        // Gives hint if guess is grater or less than the number
+        if ( guess > number ){
+            cout << "Wrong. Your guess is greater than the number" << endl;
+        }
+        
+        else if ( guess < number ){
+            cout << "Wrong. Your guess is less than the number" << endl;
+        }
+    }
+    
+    cout << "You guessed" << endl;
+}
