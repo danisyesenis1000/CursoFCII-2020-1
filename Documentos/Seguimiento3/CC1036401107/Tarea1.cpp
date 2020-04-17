b19a961be4786afd3dc1599d827022aa1f2791eb
elime920 <53834626+elime920@users.noreply.github.com>
2020-04-16 22:36:46 -0500

    Update Tarea1.cpp

--- Documentos/Seguimiento3/CC1036401107/Tarea1.cpp
+++ Documentos/Seguimiento3/CC1036401107/Tarea1.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-void Nomina( char [][10], char [][20], float*, float*); //Función prototipo.
+void Nomina( char [][10], char [][20], float*, float*, float*); //Función prototipo.
 
 int main()
 {
@@ -23,6 +23,7 @@ int main()
   char Tarifas[4][20] = {"555-88-2222", "555-77-4444", "555-77-6666", "555-99-8888"};  // Arreglo con el número de seguridad social
   float Horas[] = {7.32, 8.32, 6.54, 9.80};   // Arreglo de la tarifa por horas.
   float Trabajadas[] = {37, 40,40,35};     // Arreglo de horas trabajadas.
+  float Pago[] = {0,0,0,0};    // Arreglo donde va a ir la nomina
 
   for(int i=0; i<4; i++)     // ciclo que corre sobre todo el arreglo para que haga la tabla en el archivo de texto.
     archivoCreado << Nombres[i] << ' ' << Tarifas[i]
@@ -35,7 +36,7 @@ int main()
        << fixed << showpoint;
 
 
-  Nomina( Nombres, Tarifas, Horas, Trabajadas);    
+  Nomina( Nombres, Tarifas, Horas, Trabajadas, Pago);    
   
   return 0;
   
@@ -49,7 +50,7 @@ el número de seguridad social y la nomina.
 
 */
 
-void Nomina(char Nombres[][10], char Tarifas[][20], float * Horas, float * Trabajadas)  
+void Nomina(char Nombres[][10], char Tarifas[][20], float * Horas, float * Trabajadas, float * Pago)  
 {
   ifstream AbrirArchivo("Lista.txt");   // Para leer el archivo
 
@@ -59,14 +60,14 @@ void Nomina(char Nombres[][10], char Tarifas[][20], float * Horas, float * Traba
       exit( 1 );
     }
 
-  float *Pago;
+  
 
   for (int i = 0; i < 4; i++)      // Ciclo que saca la nomina y despliega en pantalla.
     {
       Pago[i] = Horas[i] * Trabajadas[i];
       cout << left << setw( 10 ) << Nombres[i] << setw( 13 )
 	   << Tarifas[i] << setw( 10 ) << setprecision( 2 ) << right
-	   << Pago << endl;
+	   << Pago[i] << endl;
     }
   
 }
